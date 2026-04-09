dbd96809c2c7a44482002c1d542f1999255e0351
Ivan Vostrecov <ivan.betep@gmail.com>
2026-04-09 21:08:09 +0300

    - Added a user model to the admin panel with customizable filters and search. - Necessary changes have been made to the settings.
    - Добавлена модель пользователя в административной панели с настраиваемыми фильтрами и поиском.
    - Внесены необходимые изменения в настройки.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # Что отображаем в списке
+    list_display = ('id', 'email', 'last_name', 'first_name', 'role', 'is_active')
+
+    # Что можно редактировать прямо в списке
+    list_editable = ('role', 'is_active')
+
+    # По каким полям фильтруем (боковая панель)
+    list_filter = ('role', 'is_active')
+
+    # По каким полям ищем (сверху появится строка поиска)
+    search_fields = ('email', 'last_name', 'first_name', 'phone')
